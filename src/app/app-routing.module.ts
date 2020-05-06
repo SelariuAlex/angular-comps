ec1a75f0aa4604d5dd3ffe422e6f7edb10a7e5f4
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-05-06 12:14:31 +0300

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,27 @@ import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
   {
+    path: "collections",
+    loadChildren: () =>
+      import("./collections/collections.module").then(
+        (m) => m.CollectionsModule
+      ),
+  },
+  {
+    path: "views",
+    loadChildren: () =>
+      import("./views/views.module").then((m) => m.ViewsModule),
+  },
+  {
+    path: "mods",
+    loadChildren: () => import("./mods/mods.module").then((m) => m.ModsModule),
+  },
+  {
+    path: "elements",
+    loadChildren: () =>
+      import("./elements/elements.module").then((m) => m.ElementsModule),
+  },
+  {
     path: "",
     component: HomeComponent,
   },
